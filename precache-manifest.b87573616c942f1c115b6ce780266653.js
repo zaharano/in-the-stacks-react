fe590ef7ce49329b8801d1f0f5caaa69a50a7e89
Zachary Rais-Norman <zraisnorman@gmail.com>
2020-03-02 13:42:37 -0500

    Updates

---
+++ precache-manifest.b87573616c942f1c115b6ce780266653.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065e45f02f6d63c9ae803a20bf8a395c",
+    "url": "/in-the-stacks-react/index.html"
+  },
+  {
+    "revision": "11ff630705b57ab27427",
+    "url": "/in-the-stacks-react/static/css/main.2fdb803c.chunk.css"
+  },
+  {
+    "revision": "a7ded9ebf2c50471428c",
+    "url": "/in-the-stacks-react/static/js/2.7925476a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/in-the-stacks-react/static/js/2.7925476a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11ff630705b57ab27427",
+    "url": "/in-the-stacks-react/static/js/main.174a125c.chunk.js"
+  },
+  {
+    "revision": "5ca0fc669353652727b2",
+    "url": "/in-the-stacks-react/static/js/runtime-main.6823e43e.js"
+  }
+]);
